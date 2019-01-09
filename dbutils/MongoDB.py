b0c3e12f64ff1382907eb87e4afb64c18cf50fc3
Cason <answerczy@qq.com>
2019-01-09 23:17:15 +0800

    相对及绝对导入包的区别

--- dbutils/MongoDB.py
+++ dbutils/MongoDB.py
@@ -1,3 +1,3 @@
 import dbutils.sqlite  #绝对导入
 from . import sqlite   #相对导入，.表示当前位置导入全部的sqlite
-from .sqlite import db_url #相对导入，只能使用在from里面，表示导入模块下的一个内容
+from .sqlite import db_url #相对导入，只能使用在from里面，表示导入模块下的一个内容。
